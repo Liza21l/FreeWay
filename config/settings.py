4b95bf6535c8f03877050974fe470ced95126f1f
Liza21l <Liza2009koshenya@gmail.com>
2026-06-08 14:25:52 +0300

    update

--- config/settings.py
+++ config/settings.py
@@ -23,19 +23,26 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "freeway-production.up.railway.app"
+]
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["freeway-production.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = [
     "https://freeway-production.up.railway.app"
 ]
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Application definition
 
